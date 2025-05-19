f80f36d536a529cbf5a43c19b16d0b39bef66d0b
Nightclaus <159602138+Nightclaus@users.noreply.github.com>
2025-05-19 21:10:03 +1000

    Added C# version

---
+++ c1_cs/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int maxInt = int.Parse(Console.ReadLine());
+        int radius = maxInt / 2;
+        List<string> array = new List<string>();
+
+        for (int i = 0; i < maxInt; i++)
+        {
+            string line = Console.ReadLine();
+            array.Add(line);
+        }
+
+        int result = 0;
+        for (int i = 0; i < radius; i++) {
+            if (array[i] == array[i + radius])
+            {
+                result += 2;
+            }
+        }
+        Console.WriteLine(result);
+    }
+}
