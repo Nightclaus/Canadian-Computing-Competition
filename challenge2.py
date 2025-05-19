a24a26a2592a0914fad0becb841bd70fb9edd3b6
Nightclaus <159602138+Nightclaus@users.noreply.github.com>
2025-05-19 16:46:00 +1000

    Refactored into more functional code, fixed blind acceptance of double repeating arrays (HHHH is not allowed) through a XOR operator

--- challenge2.py
+++ challenge2.py
@@ -1,45 +1,35 @@
 ## CCC Challenge 2 ##
 
-def isSame(array):
-    if len(array) < 2:
-        return False, None ## Too small
-    if len(set(array)) == 1:
-        return True, array[0]
-    return False, None
+def getHeavyLetter(array):
+    if (len(array) >= 2) and (len(set(array)) == 1): # Needs at least 2 values to be considered repeating | Check if there is only one unique value (repeating) 
+        return array[0]
+    return None
 
 def getInput():
     userInput = input()
     listOfArgs = userInput.split()
     try: 
-        amountOL= int(listOfArgs[0])
-        lengthOL = int(listOfArgs[1]) # Unused bc we using set
+        return int(listOfArgs[0]), int(listOfArgs[1]) # Amount of lines [T] | Expected length of each line [N] (unused)
     except:
         raise ValueError("Numbers must be integers")
 
-    # OL := ___ of Lines
-    return amountOL, lengthOL
+def hasRepeatWithoutIntersection(array, disjointArray):
+    heavyLetter = getHeavyLetter(array)
+    if heavyLetter and (not heavyLetter in disjointArray):
+        return True
+    return False
 
-def challenge2(amountOL, lengthOL):
-    output = ""
-    for _ in range(amountOL):
-        line = input()
-        #print(line[::2])
-        array1 = line[::2]
-        array2 = line[1::2]
+def process(line):
+    # Parse Inputs
+    array1 = line[::2]
+    array2 = line[1::2]
 
-        bool1, value1 = isSame(array1)
-        bool2, value2 = isSame(array2)
-        if bool1:
-            if not value1 in array2: ## Check if consecuatuve
-                output+="T\n" # True
-                continue
-        elif bool2:
-            if not value2 in array1: ## Check if consecuatuve
-                output+="T\n" # True
-                continue
-        output+="F\n" # False
+    if (hasRepeatWithoutIntersection(array1, array2) ^ hasRepeatWithoutIntersection(array2, array1)): # XOR distinction, both cannot be alternating heavies
+        return "T"  # True
+    return "F"      # False
 
-    print(output.removesuffix("\n")) # Avoid having to iterate if it was an array instead
-
-a,b = getInput()
-challenge2(a,b)
+output = "" # Charbuilder
+t,n = getInput()
+for _ in range(t):
+    output += process(input()) + '\n'
+print(output.removesuffix('\n')) 
